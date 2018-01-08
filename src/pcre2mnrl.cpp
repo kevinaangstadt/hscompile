901edbf1d6958fd5cdd02c0f4e1814c33a825551
Jack Wadden <jackwadden@gmail.com>
2018-01-08 18:59:29 -0500

    Cleaned up pcre2mnrl parser so it's easier to diagnose what's *wrong* with PCREs and where errors are in the PCRE to automata stack.

--- src/pcre2mnrl.cpp
+++ src/pcre2mnrl.cpp
@@ -9,6 +9,12 @@
 
 using namespace std;
 
+char *strdup(const char *src_str) noexcept {
+    char *new_str = new char[std::strlen(src_str) + 1];
+    std::strcpy(new_str, src_str);
+    return new_str;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         cerr << "Usage: " << argv[0] << " <regex file> <mnrl file>" << endl;
@@ -27,28 +33,26 @@ int main(int argc, char *argv[]) {
     unsigned i = 0;
     while(getline(infile, line)) {
         
-        vector<string> line_tokens;
-        boost::algorithm::split_regex(line_tokens, line, boost::regex("^/|/(?=[^/]*$)"), boost::match_default);
-        
+        size_t start_loc = line.find_first_of("/");
+        size_t end_loc = line.find_last_of("/");
         
         
         // check that we have 3 parts (i.e. 2 / characters)
-        if(line_tokens.size() != 3) {
+        if(start_loc == string::npos || end_loc == string::npos){
             cerr << "Rule on line " << i+1 << " was not surrounded by slashes!" << endl;
             exit(5);
         }
         
-        /*
-         * line_tokens[0] == empty
-         * line_tokens[1] == the expression
-         * line_tokens[2] == the modifiers
-         */
-        
+        string expr = line.substr(start_loc + 1, end_loc - 1);
+        //cout << "EXPR: " << expr << endl;
+        string mods = line.substr(end_loc + 1, line.size());
+        //cout << "MODS: " << mods << endl;
+
         // convert the modifiers
         int e_flags = 0;
         bool failed = false;
-        if(line_tokens[2].size() > 0) {
-            for(char &m : line_tokens[2]) {
+        if(mods.size() > 0) {
+            for(char &m : mods) {
                 // PAY ATTENTION note that we are ORing things together
                 // DON'T FORGET A BREAK
                 switch(m) {
@@ -61,9 +65,12 @@ int main(int argc, char *argv[]) {
                 case 's':
                     e_flags |= HS_FLAG_DOTALL;
                     break;
+                case 'H':
+                    e_flags |= HS_FLAG_SINGLEMATCH;
+                    break;
                 default:
                     failed = true;
-                    cerr << "Unsupported modifier '" << m << "' on line " << i+1 << endl;
+                    //cerr << "Unsupported modifier '" << m << "' on line " << i+1 << endl;
                 }
             }
         }
@@ -71,28 +78,32 @@ int main(int argc, char *argv[]) {
         
         // if we failed to parse, continue
         if(failed) {
-            cerr << "Rule on line " << i+1 << " had unsupported modifiers, skipping" << endl;
+            //cerr << "Rule on line " << i+1 << " had unsupported modifiers, skipping" << endl;
         } else {
-            expressions.push_back(line_tokens[1]);
-            cexpressions.push_back(expressions.back().c_str());
+            expressions.push_back(expr);
+            const char *cexpr = strdup(expr.c_str());
+            cexpressions.push_back(cexpr);
             ids.push_back(i);
             flags.push_back(e_flags);
         }
         
         i++;
+        
     }
+
     
     hs_compile_error_t *compile_err;
     MNRL::MNRLNetwork mnrl("pcre");
-    
-    
+
     if (hs_pcre_mnrl_multi(cexpressions.data(),
                      mnrl,
                      &flags[0],
                      &ids[0],
                      cexpressions.size(),
                      &compile_err) != HS_SUCCESS) {
-        cerr << "ERROR: Unable to compile PCRE expressions file " << argv[1] << ": " << compile_err->message << endl;
+        cerr << "ERROR: Unable to compile PCRE expression file " << argv[1] << " expression number " << compile_err->expression << " : " << compile_err->message << endl;
+        cerr << " pcre: " << expressions[compile_err->expression] << endl;
+        cerr << " flags: " << flags[compile_err->expression] << endl;
     }
     
     mnrl.exportToFile(argv[2]);
